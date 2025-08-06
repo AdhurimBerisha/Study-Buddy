4f7ff524d47326bc941df19a7fca0d5339e982a4
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-08-06 15:08:36 +0200

    TutorsCard & TutorsGrid

--- frontend/src/pages/Tutors/Tutors.tsx
+++ frontend/src/pages/Tutors/Tutors.tsx
@@ -1,9 +1,11 @@
 import Hero from "../../components/Hero";
+import TutorsGrid from "./TutorsGrid";
 
 const Tutors = () => {
   return (
     <div>
       <Hero />
+      <TutorsGrid />
     </div>
   );
 };
--- frontend/src/pages/Tutors/TutorsGrid.tsx
+++ frontend/src/pages/Tutors/TutorsGrid.tsx
@@ -1,4 +1,43 @@
+import TutorCard from "./TutorsCard";
+import { FaUserCircle } from "react-icons/fa";
+
+const tutors = [
+  {
+    avatar: <FaUserCircle />,
+    category: "English Language",
+    rating: 4.9,
+    lessons: 125,
+    name: "Jaxon Clarke",
+    headline: "Native English Teacher from the U.S.",
+    description:
+      "consectetur adipisicing elit, sed do eiusmod ut labore et magna aliqua...",
+    speaks: "English (Native), French C1",
+    hourlyRate: "USD 15.50",
+    trialRate: "USD 6.50",
+  },
+  {
+    avatar: <FaUserCircle />,
+    category: "English Language",
+    rating: 4.3,
+    lessons: 100,
+    name: "Amilia Luna",
+    headline: "Native English Teacher from the U.S.",
+    description:
+      "consectetur adipisicing elit, sed do eiusmod ut labore et magna aliqua...",
+    speaks: "English (Native)",
+    hourlyRate: "USD 14.00",
+    trialRate: "USD 6.00",
+  },
+];
+
 const TutorsGrid = () => {
-  return <div>TutorsGrid</div>;
+  return (
+    <section className="max-w-5xl mx-auto px-6 flex flex-col items-center">
+      {tutors.map((tutor, index) => (
+        <TutorCard key={index} {...tutor} />
+      ))}
+    </section>
+  );
 };
+
 export default TutorsGrid;
