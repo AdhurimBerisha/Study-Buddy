147d26f0b626b12c9b1fabf23dddbc778efff81d
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-08-06 23:15:28 +0200

    Contact Page & Button styling

--- frontend/src/pages/Contact/Contact.tsx
+++ frontend/src/pages/Contact/Contact.tsx
@@ -1,9 +1,19 @@
+import Banner from "../../components/Banner";
 import Hero from "../../components/Hero";
+import ContactForm from "./ContactForm";
+import bannerBg from "../../assets/bannerBg.webp";
 
 const Contact = () => {
   return (
     <div>
       <Hero />
+      <ContactForm />
+      <Banner
+        imageSrc={bannerBg}
+        title="Start learning a new language today!"
+        subtitle="Choose a teacher for 1-on-1 lessons"
+        buttonText="Sign Up"
+      />
     </div>
   );
 };
