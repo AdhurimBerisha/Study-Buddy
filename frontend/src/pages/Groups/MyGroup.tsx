d92e1cbf2361fa7542367b32a9dd9d1083f3e986
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-08-06 22:51:43 +0200

    Added MyGroup

---
+++ frontend/src/pages/Groups/MyGroup.tsx
@@ -0,0 +1,13 @@
+const YourGroup = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Your Group</h1>
+      <p>
+        This page will show details or content related to the group you belong
+        to.
+      </p>
+    </div>
+  );
+};
+
+export default YourGroup;
