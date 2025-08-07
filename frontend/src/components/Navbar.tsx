80f64b2b84145fbe04a30bc7e58251e110a86b79
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-08-07 23:09:53 +0200

    GroupDetails Page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -82,7 +82,7 @@ const Navbar = () => {
             </li>
             <li>
               <NavLink
-                to="/groups/my-group"
+                to="/groups/my"
                 className="block px-4 py-2 hover:bg-gray-100 w-full"
               >
                 Your Groups
