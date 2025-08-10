950232f484bec54f2524ccb44baab70f04a447ec
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-08-10 14:58:04 +0200

    Database Connection

---
+++ backend/config/db.ts
@@ -0,0 +1,20 @@
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const connectMySql = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("MySQL Connected Successfully");
+    connection.release();
+  } catch (error: any) {
+    console.log("MySQL Connection Failed", error.message);
+  }
+};
+
+export { pool, connectMySql };
--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { connectMySql } from "./config/db";
 
 const app = express();
 const PORT = 8080;
@@ -10,6 +12,8 @@ app.use(express.json());
 app.get("/", (req, res, next) => {
   res.send("Server is running!");
 });
+
+connectMySql();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
