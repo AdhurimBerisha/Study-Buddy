b6c593159975acd78c4156a94b5ce87f292ac21a
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-08-25 15:25:45 +0200

    updateProgress

--- backend/routes/lesson.ts
+++ backend/routes/lesson.ts
@@ -4,6 +4,7 @@ import {
   editLesson,
   deleteLesson,
   getCourseProgress,
+  updateLessonProgress,
 } from "../controllers/lessonController";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.put("/:id", requireAuth, editLesson);
 router.delete("/:id", requireAuth, deleteLesson);
 router.get("/course/:courseId/progress", requireAuth, getCourseProgress);
+router.put("/:lessonId/progress", requireAuth, updateLessonProgress);
 
 export default router;
