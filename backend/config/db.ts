aebe8f1e9038bbadc1b2175dcb74a63730dabc54
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-11-11 17:56:41 +0100

    fIX

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,18 +1,26 @@
-// db.ts
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.MYSQL_URL as string, {
-  dialect: "mysql",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: true, // required for Aiven
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE!,
+  process.env.MYSQL_USER!,
+  process.env.MYSQL_PASSWORD!,
+  {
+    host: process.env.MYSQL_HOST!,
+    port: Number(process.env.MYSQL_PORT!),
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+      },
     },
-  },
-  define: {
-    timestamps: true,
-  },
-});
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => console.log("✅ Database connected successfully!"))
+  .catch((err) => console.error("❌ Error connecting to DB:", err));
 
 export default sequelize;
